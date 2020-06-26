45e91edeb49a9dffc0229a75598386bfdde4909f
Fleshborne <blakeathompson94@gmail.com>
2020-06-26 06:29:28 -0400

    server setup

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const PORT = process.env.PORT || 3306;
+const PORT = process.env.PORT || 8080;
 const exphbs = require("express-handlebars");
 const app = express();
 
