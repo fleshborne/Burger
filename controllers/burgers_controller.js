ac065d45a73fed39b018d79d06e1e3dd360be3e4
Fleshborne <blakeathompson94@gmail.com>
2020-06-26 06:13:33 -0400

    server setup

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,9 +2,6 @@ const router = require("express").Router();
 const Burger = require("../models/burger.js");
 const burger = new Burger();
 
-router.use(express.urlencoded({ extended: true }));
-router.use(express.json());
-
 router.get("/", (req, res) => {
   // burger selectAll here
   burger.selectAll().then();
