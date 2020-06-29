865a75f5c1095aed190131d3f0fae017cb316713
Fleshborne <blakeathompson94@gmail.com>
2020-06-29 10:40:48 -0400

    controllers finished

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,11 +4,55 @@ const burger = new Burger();
 
 router.get("/", (req, res) => {
   // burger selectAll here
-  burger.selectAll().then();
+  burger.all((data) => {
+    const hdbrsObject = {
+      burgers: data,
+    };
+    console.log(hdbrsObject);
+    res.render("index", hdbrsObject);
+  });
+});
 
-  // render the response
+router.post("/api/burgers", (req, res) => {
+  burger.create(
+    ["burger_name", "devoured"],
+    [req.body.burger_name, req.body.devoured],
+    (result) => {
+      res.json({ id: result.insertId });
+    }
+  );
+});
 
-  // catch error
+router.put("api/burgers/:id", (req, res) => {
+  let condition = "id = " + req.params.id;
+  console.log("condition", condition);
+
+  burger.update(
+    {
+      devoured: req.body.devoured,
+    },
+    condition,
+    (result) => {
+      if (result.changedRows == 0) {
+        // if no rows were changed, then the ID must bot exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    }
+  );
+});
+
+router.delete("api/burgers/:id", (req, res) => {
+  const condition = "id = " + req.params.id;
+
+  burger.delete(condition, (result) => {
+    if (result.affectedRows == 0) {
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
 });
 
 module.exports = router;
